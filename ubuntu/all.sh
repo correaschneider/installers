30b91600d475e28136090822b8d08a49833fb726
Pedro Schneider <correaschneider@gmail.com>
2018-03-25 20:43:16 -0300

    Instalador de todos

--- ubuntu/all.sh
+++ ubuntu/all.sh
@@ -4,8 +4,10 @@ sudo chmod +x docker.sh
 sudo chmod +x node.sh
 sudo chmod +x robomongo.sh
 sudo chmod +x sublime.sh
+sudo chmod +x mongo.sh
 
 sudo ./docker.sh
 sudo ./node.sh
 sudo ./robomongo.sh
 sudo ./sublime.sh
+sudo ./mongo.sh
