29577d16e5354b93b43d3e70326e94ee91f9ec42
Pedro Schneider <correaschneider@gmail.com>
2018-03-25 20:18:08 -0300

    Reestruturando installers

---
+++ ubuntu/all.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+sudo chmod +x docker.sh
+sudo chmod +x node.sh
+sudo chmod +x robomongo.sh
+sudo chmod +x sublime.sh
+
+sudo ./docker.sh
+sudo ./node.sh
+sudo ./robomongo.sh
+sudo ./sublime.sh
